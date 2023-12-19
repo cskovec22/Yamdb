69e606046ff077e5dad63720416eaa76c90d0e52
Никита Васин <cskovec22@yandex.ru>
2023-12-19 15:48:02 +0700

    В файле README.md добавлена информация о проекте, исправлены ошибки flake8.

--- api_yamdb/reviews/management/commands/importcsv.py
+++ api_yamdb/reviews/management/commands/importcsv.py
@@ -1,15 +1,27 @@
 import csv
 from typing import Any
-from django.core.management.base import BaseCommand
 
-from reviews.models import Category, Comment, GenreTitle, Genre, Review, Title, CustomUser
+from django.core.management.base import BaseCommand
+from reviews.models import (Category, Comment, CustomUser, Genre, GenreTitle,
+                            Review, Title)
 
 
 class Command(BaseCommand):
     help = 'Импортирует данные из csv в базу данных'
 
     def handle(self, *args: Any, **options: Any) -> str | None:
-        with open('static/data/category.csv', newline='', encoding='utf-8') as csvfile:
+        self.import_categories()
+        self.import_users()
+        self.import_genres()
+        self.import_titles()
+        self.import_genre_title()
+        self.import_review()
+        self.import_comments()
+
+    def import_categories(self):
+        with open(
+            'static/data/category.csv', newline='', encoding='utf-8'
+        ) as csvfile:
             reader = csv.reader(csvfile, delimiter=',')
             for count, row in enumerate(reader):
                 if count == 0:
@@ -17,7 +29,10 @@ class Command(BaseCommand):
                 Category.objects.create(id=row[0], name=row[1], slug=row[2])
             self.stdout.write(self.style.SUCCESS('Категории загружены'))
 
-        with open('static/data/users.csv', newline='', encoding='utf-8') as csvfile:
+    def import_users(self):
+        with open(
+            'static/data/users.csv', newline='', encoding='utf-8'
+        ) as csvfile:
             reader = csv.reader(csvfile, delimiter=',')
             for count, row in enumerate(reader):
                 if count == 0:
@@ -33,6 +48,7 @@ class Command(BaseCommand):
                 )
             self.stdout.write(self.style.SUCCESS('Пользователи загружены'))
 
+    def import_genres(self):
         with open(
             'static/data/genre.csv',
             newline='',
@@ -45,6 +61,7 @@ class Command(BaseCommand):
                 Genre.objects.create(id=row[0], name=row[1], slug=row[2])
             self.stdout.write(self.style.SUCCESS('Жанры загружены'))
 
+    def import_titles(self):
         with open(
             'static/data/titles.csv',
             newline='',
@@ -62,6 +79,7 @@ class Command(BaseCommand):
                 )
             self.stdout.write(self.style.SUCCESS('Произведения загружены'))
 
+    def import_genre_title(self):
         with open(
             'static/data/genre_title.csv',
             newline='',
@@ -80,6 +98,7 @@ class Command(BaseCommand):
                 'Таблица Genre-title загружена'
             ))
 
+    def import_review(self):
         with open(
             'static/data/review.csv',
             newline='',
@@ -99,6 +118,7 @@ class Command(BaseCommand):
                 )
             self.stdout.write(self.style.SUCCESS('Отзывы загружены'))
 
+    def import_comments(self):
         with open(
             'static/data/comments.csv',
             newline='',
